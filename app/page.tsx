a7b56388fc4a35904fc626b413a2d92bf4f84101
alesm10 <154551922+alesm10@users.noreply.github.com>
2026-01-14 21:02:25 +0100

    Update page.tsx to use Next.js redirect to /retrokredenc/

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-  return (
-    <div>
-      <h1>Retrokredenc Shop</h1>
-      <p>Redirect to main content...</p>
-      <script dangerouslySetInnerHTML={{
-        __html: 'window.location.href = "/retrokredenc/index.html";'
-      }} />
-    </div>
-  );
+  redirect('/retrokredenc/');
 }
